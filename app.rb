86d4ad05b42203e66a342108a205912416557b34
remote-torrent-6747 <abbyc@sky.com>
2021-04-11 11:02:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,17 @@ class App < Sinatra::Base
 
   post '/newteam' do
     @team_name = params["team_name"]
+    @coach = params["coach"]
+    @point_guard = params["point_guard"]
+    @shooting_guard = params["shooting_guard"]
+    @coach = params["coach"]
+    @coach = params["coach"]
     erb :team
   end
 
 end
+
+# <p>Shooting Guard: <input type="text" name=""></p>
+# <p>Small Forward: <input type="text" name="small_forward"></p>
+# <p>Power Forward: <input type="text" name="power_forward"></p>
+# <p>Center: <input type="text" name="center"></p> -->
